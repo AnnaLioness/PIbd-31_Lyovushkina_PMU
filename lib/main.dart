f9a230d93f43eaaa40839e1245afcb3d0ded1dfc
annalyovushkina@yandex.ru <annalyovushkina@yandex.ru>
2024-09-30 12:10:09 +0400

    лаб3 в процессе

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-                                                                                                                                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Лёвушкина Анна Александровна'),
@@ -23,8 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-      
-        
   final String title;
 
   @override
@@ -32,39 +30,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-                                    _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const MyWidget(),
+    );
   }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-                            return Scaffold(
-      appBar: AppBar(
-                                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                        title: Text(widget.title),
+    return Center(
+      child: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MyCardWidget('hi' *3),
+          MyCardWidget('nice ass!)'),
+          MyCardWidget('джуси пуси на тусе'),
+        ],
       ),
-      body: Center(
-                        child: Column(
-                                                                                                                                            mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+    ),
+    );
+  }
+}
+
+class MyCardWidget extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  const MyCardWidget(
+    this.text, {
+        this.icon = Icons.ac_unit_outlined,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.cyanAccent,
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: Colors.indigo,
+          width: 4,
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),     );
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Text(
+              text,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ),
+          Icon(icon),
+        ],
+      ),
+    );
   }
 }
