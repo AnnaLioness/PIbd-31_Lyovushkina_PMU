6b234c3762b1f0fd58f7d2e7be51f2d754b304a0
annalyovushkina@yandex.ru <annalyovushkina@yandex.ru>
2024-11-26 04:08:43 +0400

    лаб7 готова

---
+++ lib/components/resources.g.dart
@@ -0,0 +1,12 @@
+/// Generate by [asset_generator](https://github.com/fluttercandies/flutter_asset_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+// ignore_for_file: constant_identifier_names
+class R {
+  const R._();
+
+  /// ![preview](file://C:\Users\annal\StudioProjects\pmu_labs\assets\svg\ru.svg)
+  static const String ASSETS_SVG_RU_SVG = 'assets/svg/ru.svg';
+
+  /// ![preview](file://C:\Users\annal\StudioProjects\pmu_labs\assets\svg\uk.svg)
+  static const String ASSETS_SVG_UK_SVG = 'assets/svg/uk.svg';
+}
